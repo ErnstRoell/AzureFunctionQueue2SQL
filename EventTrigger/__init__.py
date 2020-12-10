21895f6bbd191ca27010e3b65773f0a568723a93
Ernst <erroell@microsoft.com>
2020-12-10 10:35:45 +0100

    added example localsettings

--- EventTrigger/__init__.py
+++ EventTrigger/__init__.py
@@ -11,30 +11,47 @@ import os
 def main(msg: func.QueueMessage) -> None:
     logging.info('Python queue trigger function processed a queue item: %s',
                  msg.get_body().decode('utf-8'))
+
+    # Fetch passwords and connection strings from the environment variables
+    # Add your Computer Vision subscription key and endpoint to your environment variables.
+    if 'DB_CONNECTION_STRING' in os.environ:
+        db_connection_string = os.environ['DB_CONNECTION_STRING']
+    else:
+        logging.info("\nSet the DB_CONNECTION_STRING environment variable.\n Please add it to your local.settings.json.\n**Restart your shell or IDE for changes to take effect.**")
+        sys.exit()
     
-    conn = pypyodbc.connect("Driver={ODBC Driver 17 for SQL Server};Server=tcp:jumboserver.database.windows.net,1433;Database=jumbo-simacan-db;Uid=jumboadmin;Pwd=ebbbcc6c-998d-48dc-88b0-c5541fdf16c9;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;")
+    # Here we create the connection to the database and initialize the     
+    conn = pypyodbc.connect(db_connection_string)
     c = conn.cursor()
 
+    # Database name
     name = 'simacan'
 
+    # Fetch the data from the message body 
     data = json.loads(msg.get_body().decode('utf-8'))
 
+    # Do first round of normalisation 
     df = pd.json_normalize(data,meta=['id','name','ppu','batters','type'],record_prefix='topping_', record_path=['topping'])
     cols = df.columns.values
     cols = [c for c in cols if c!='batters']
+
+    # Normalize second array (repeat steps if more arrays exist)
     d2 = json.loads(df.to_json(orient='records'))
     df = pd.json_normalize(d2,meta=cols,record_prefix='batter_', record_path=['batters','batter'])
 
-    schema = pd.io.sql.get_schema(df, name)
-    logging.info(schema)
+    # # Create the SQL query to initialize the database schema from the inferred datatypes in the DataFrame
+    # schema = pd.io.sql.get_schema(df, name)
+    # logging.info(schema)
+    # # Send the query to the database
     # c.execute(schema)
+
     # Insert Dataframe into SQL Server:
     for index, row in df.iterrows():
         logging.info("INSERT INTO {} ({}) values({})".format(name,",".join(df.columns),"'"+"','".join(row.astype(str))+"'"))
         try:
             c.execute("INSERT INTO {} ({}) values({})".format(name,",".join(df.columns),"'"+"','".join(row.astype(str))+"'"))
-        except IntegrityError as e:
-            logging.info('IntegrityError') 
+        except IntegrityError as e: # Integrity error indicates duplicates exist. 
+            logging.info('IntegrityError, trying an update') 
             vals = ",".join([col+" = " + "'" + str(val) + "'" for col,val in zip(df.columns,row.astype(str))])
             keys = " and ".join([col+' = '+str(val) for col,val in zip(['id','topping_id','batter_id'],[str(row.id),str(row.topping_id),str(row.batter_id)])])
             logging.info(f"UPDATE {name} SET {vals} WHERE {keys}")
